3102b739763ec01a02cdc1a7a90157f41ca8e670
Georgios Chaniotakis <haniotinho@gail.com>
2019-09-18 11:55:56 +0300

    first simple tests in registration feature

---
+++ BDDExampleTests/Registration/ValidApplicationReceived.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BDDExampleTests.Registration
+{
+    [Trait("A Valid Application is Submitted", "")]
+    public class ValidApplicationReceived
+    {
+        [Fact(DisplayName ="A user is added to the system")]
+        public void UserIsAddedToSystem()
+        {
+            throw new NotImplementedException("Implement me");
+        }
+
+        [Fact(DisplayName ="User status set to pending")]
+        public void UserStatusSetToPending()
+        {
+            throw new NotImplementedException("Implement me");
+        }
+
+        [Fact(DisplayName ="Log entry created for event")]
+        public void LogEntryIsCreatedForEvent()
+        {
+            throw new NotImplementedException("Implement me");
+        }
+
+        [Fact(DisplayName ="Email is sent to confirm address")]
+
+        public void EmailSentToConfirmAddress()
+        {
+            throw new NotImplementedException("Implement me");
+        }
+
+        [Fact(DisplayName ="A confirmation message is provided to show to the user")]
+
+        public void AMessageIsProvidedForUser()
+        {
+            throw new NotImplementedException("Implement me");
+        }
+
+       
+    }
+}
