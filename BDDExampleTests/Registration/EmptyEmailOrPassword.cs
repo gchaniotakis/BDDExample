c8e8cdec7eff1d991a22a596540b85c71054fe09
Georgios Chaniotakis <haniotinho@gail.com>
2019-09-18 12:57:01 +0300

    new classes +runner

---
+++ BDDExampleTests/Registration/EmptyEmailOrPassword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BDDExampleTests.Registration
+{
+    [Trait("An application is received with empty email or password", "")]
+    public class EmptyEmailOrPassword
+    {
+        [Fact(DisplayName = "Application considered invalid")]
+        public void ApplicationInvalid()
+        {
+            throw new NotImplementedException("Implement me");
+        }
+
+        [Fact(DisplayName = "A message is returned")]
+        public void MessageReturned()
+        {
+            throw new NotImplementedException("Implement me");
+        }
+    }
+}
---
+++ BDDExampleTests/Registration/InvalidApplicationReceived.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BDDExampleTests.Registration
+{
+    [Trait("An Invalid Application was received", "")]
+    public class InvalidApplicationReceived
+    {
+        [Fact(DisplayName = "Application is Denied")]
+        public void UserDenied()
+        {
+            throw new NotImplementedException("Implement me");
+        }
+
+        [Fact(DisplayName = "A message is shown explaining why")]
+        public void MessageIsShown()
+        {
+            throw new NotImplementedException("Implement me");
+        }
+    }
+}
