db5084af060084be296b0b8eed2d4a682e5c0778
Georgios Chaniotakis <haniotinho@gail.com>
2019-09-19 11:40:54 +0300

    new project added/first migration

---
+++ TaskRunner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BDDExample.DB;
+using BDDExample.Models;
+
+namespace TaskRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbcontext = new ApplicationDbContext();
+            var user = new User { Email="g.chaniotakis@bewise.gr"};
+            Console.WriteLine(user.Id);
+
+            dbcontext.Users.Add(user);
+            dbcontext.SaveChanges();
+            Console.WriteLine("Done!");
+            Console.Read();
+        }
+    }
+}
